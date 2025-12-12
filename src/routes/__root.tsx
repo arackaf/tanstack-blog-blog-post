2edf2f7e80c079be5d04745c4a919b72d1188b2a
Adam Rackis <adamrackis@hotmail.com>
2025-12-11 18:00:36 -0600

    root layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -36,7 +36,13 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
-        {children}
+        <main className="flex flex-col max-w-[708px] mx-auto px-4">
+          <article>
+            <section>
+              <div className="mt-4 sm:mt-10">{children}</div>
+            </section>
+          </article>
+        </main>
         <TanStackDevtools
           config={{
             position: "bottom-right",
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -34,7 +34,7 @@ export const Route = createFileRoute("/")({
 
 const PersonalLink: FC<PropsWithChildren<{ href: string }>> = ({ href, children }) => {
   return (
-    <a href={href} className="flex items-center sm:gap-0.5 [&>:first-child]:w-4.5 [&_svg]:fill-(--link-color) sm:[&_svg]:h-4 [&_svg]:h-3.5">
+    <a href={href} className="flex items-center sm:gap-0.5 *:first:w-4.5 [&_svg]:fill-(--link-color) sm:[&_svg]:h-4 [&_svg]:h-3.5">
       {children}
     </a>
   );
@@ -46,39 +46,36 @@ function App() {
     <div>
       <div className="blog-header flex mb-8">
         <div className="rounded-full overflow-hidden sm:w-[125px] sm:h-[125px] w-24 h-24 sm:min-w-[125px] sm:min-h-[125px] min-w-24 min-h-24">
-          <img alt="Profile pic" className="rounded-full sm:w-[125px] sm:h-[125px] w-24 h-24" src="/assets/home/avatar.jpg" />
+          <img alt="Profile pic" className="rounded-full sm:w-[125px] sm:h-[125px] w-24 h-24" src="/tanstack-logo.png" />
         </div>
         <div className="titles flex flex-col ml-2.5 justify-evenly">
           <div className="flex flex-col gap-1">
-            <h1 className="leading-none text-xl sm:text-2xl md:text-3xl font-bold">Strangely Typed</h1>
-            <h3 className="leading-none text-sm sm:text-lg md:text-xl font-bold">Software engineering blog by Adam Rackis</h3>
+            <h1 className="leading-none text-xl sm:text-2xl md:text-3xl font-bold">Blog Name Here</h1>
+            <h3 className="leading-none text-sm sm:text-lg md:text-xl font-bold">Personal blog by You</h3>
           </div>
           <div className="personal-links flex flex-col gap-1">
             <h4 className="leading-none sm:text-base text-sm">
-              <PersonalLink href="https://twitter.com/AdamRackis">
+              <PersonalLink href="https://twitter.com/demo">
                 <span>
                   <TwitterIcon />
                 </span>
-                <span className="font-bold sm:text-base text-sm leading-none!">adamrackis</span>
+                <span className="font-bold sm:text-base text-sm leading-none!">you</span>
               </PersonalLink>
             </h4>
             <h4 className="leading-none">
-              <PersonalLink href="https://github.com/arackaf">
+              <PersonalLink href="https://github.com/demo">
                 <span>
                   <GithubIcon />
                 </span>
-                <span className="font-bold sm:text-base text-sm leading-none!">arackaf</span>
+                <span className="font-bold sm:text-base text-sm leading-none!">you_also</span>
               </PersonalLink>
             </h4>
           </div>
         </div>
       </div>
       <div className="mb-8 flex flex-col gap-2">
-        <p>Hi, I'm Adam 👋</p>
-        <p>
-          Welcome to my blog. I usually write about web development—the React or Svelte stacks in particular—or occasionally GraphQL, databases, or
-          anything else I'm interested in.
-        </p>
+        <p>Hi! 👋</p>
+        <p>This is a proof of concept blog, built with TanStack Start.</p>
       </div>
 
       <div>
